d2d8758eb19abb1efd9d6602df1ad82abc2580a5
merlinpratheesh <merlinpratheesh@gmail.com>
2021-02-22 19:41:39 +0530

    layout design

--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { UserdataService } from '../service/userdata.service';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { FormControl, FormGroup } from "@angular/forms";
+
 @Component({
   selector: 'app-main-screen',
   templateUrl: './main-screen.component.html',
   styleUrls: ['./main-screen.component.scss']
 })
 export class MainScreenComponent implements OnInit {
+  names: FormGroup;
 
-  constructor(public developmentservice: UserdataService,private router: Router) { }
+  constructor(public developmentservice: UserdataService,private router: Router) {
+
+    this.names = new FormGroup({
+      projectName: new FormControl(),
+      description: new FormControl(),
+      creationDate: new FormControl(),
+      profileName: new FormControl(),
+      photoUrl: new FormControl(),
+      projectUid: new FormControl()
+    });
+   }
 
   ngOnInit(): void {
   }
